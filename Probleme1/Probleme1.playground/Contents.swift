7ca731f1f5ef855ca2deedd902a9af7effeed445
Grégory Da Silva <gregorydasilva@macbook-pro-de-gregory.home>
2018-04-10 21:00:19 +0200

    fixing names

--- Probleme1/Probleme1.playground/Contents.swift
+++ Probleme1/Probleme1.playground/Contents.swift
@@ -5,7 +5,7 @@ import UIKit
  Find the sum of all the multiples of 3 or 5 below 1000.
  */
 
-func solution(){
+func resolve(){
     var total = 0
     for number in 1..<1000{
         if number % 3 == 0 || number % 5 == 0 {
@@ -15,4 +15,4 @@ func solution(){
     print(total)
 }
 
-solution()
+resolve()
