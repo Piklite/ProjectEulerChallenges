4ed0398acc8dde8fe60f518713fb627f899635e7
Piklite <gregoryds14@gmail.com>
2018-04-17 09:33:13 +0200

    fixed pb6

--- Probleme6.playground/Contents.swift
+++ Probleme6.playground/Contents.swift
@@ -1,3 +1,7 @@
+/*
+ Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+
 func getSumOfSqrs(endOfRange: Int) -> Int {
     var arrayOfResults : [Int] = []
     for i in 1...endOfRange {
